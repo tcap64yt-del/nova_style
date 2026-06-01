252e94098006a1c749f38888a976a12104db0e60
sourav <t.cap64yt@gmail.com>
2026-06-01 09:36:59 +0530

    updated file

--- nova_style/staff/urls.py
+++ nova_style/staff/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path,include
 from . import views
+
 urlpatterns = [
    path('login/',views.admin_login,name='admin_login'),
-   path('admin-user-management/',views.admin_user_managment,name='admin_user_ management'),
+   path('user-management/',views.user_managment,name='user_management'),
+   path("admin/user-management/block/<int:user_id>/", views.block_user, name="block_user"),
+   path("admin/user-management/unblock/<int:user_id>/", views.unblock_user, name="unblock_user"),
 
 ]
--- nova_style/staff/views.py
+++ nova_style/staff/views.py
@@ -1,12 +1,59 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from user.models import Users
+from django.contrib import messages
+from django.contrib.auth import login ,logout,authenticate
+from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 
 
 def admin_login(request):
+    if request.method=="POST":
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        admin=authenticate(email=email,password=password)
+
+        if admin:
+            if admin.is_staff and admin.is_superuser:
+                login(request,admin)
+                return redirect("user_management")
+        else:
+            messages.error(request,"invaild credentials")
+
+    if request.is_authenticated:
+        if not request.user.is_staff:
+            return redirect('home')
+        return redirect('user_management')
+
     return render(request,'staff/admin_login.html')
 
-
-def admin_user_managment(request):
+@login_required(login_url='admin_login')
+def user_managment(request):
     users=Users.objects.all()
     total=Users.objects.count()
-    return render(request,'staff/admin_user_management.html',{"users":users,"total":total})
+    sort = request.GET.get('sort', 'all')
+    search = request.GET.get('search', '')
+
+    if sort=='newest':
+        users=users.order_by('-created_at')
+    elif sort == 'oldest':
+        users = users.order_by('created_at')
+
+    if search:
+        users=users.filter(Q(name__icontains=search) |Q(email__icontains=search))
+
+    return render(request,'staff/admin_user_management.html',{"users":users,"total":total,"sort":sort,"search":search})
+
+
+def block_user(request, user_id):
+    user = Users.objects.filter(id=user_id).first()
+    if user:
+        user.status = False
+        user.save(update_fields=["status"])
+    
+    return redirect('user_management')
+
+def unblock_user(request, user_id):
+    user = Users.objects.filter(id=user_id).first()
+    user.status = True
+    user.save()
+    return redirect("user_management")
