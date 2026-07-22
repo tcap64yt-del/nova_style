ee14aeeca37d4ee9aad3c9ecd1d629497fb7f0f5
sourav <t.cap64yt@gmail.com>
2026-07-22 07:54:16 +0530

    Update product forms

--- nova_style/product/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Category
-
-class CategoryForm(forms.ModelForm):
-    offer = forms.DecimalField(required=False)
-    class Meta:
-        model = Category
-        fields = ["name", "image_url", "offer", ]
-        error_messages={"name":{"required":"name is required."},'image_url':{"required":"image is required."}}
