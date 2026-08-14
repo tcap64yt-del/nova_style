886c82a3b30db59265729245f5dde53bb5bc8a74
sourav <t.cap64yt@gmail.com>
2026-08-14 11:42:09 +0530

    Update urls.py file

--- nova_style/product/urls.py
+++ nova_style/product/urls.py
@@ -4,5 +4,7 @@ urlpatterns = [
     path('list/',views.product_list,name="product_list"),
     path('details/<int:variant_id>/',views.product_details,name="product_details"),
     path('add-cart/',views.add_to_cart,name="add_to_cart"), 
+    path('buy-now/<int:variant_id>/',views.buy_now,name="buy_now"), 
+
 
 ]
