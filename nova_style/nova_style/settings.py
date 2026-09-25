11f4e38a308af74b7a022dbe792fe50a5685fbcb
sourav <t.cap64yt@gmail.com>
2026-09-25 10:14:49 +0530

    Update settings and add 404 page

--- nova_style/nova_style/settings.py
+++ nova_style/nova_style/settings.py
@@ -12,7 +12,7 @@ load_dotenv(BASE_DIR / ".env")
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
-DEBUG = os.getenv("DJANGO_DEBUG", "False").lower() == "true"
+DEBUG = False
 
 ALLOWED_HOSTS = [
     host.strip()
